0c7ba986064a5e8b8fda4d3735ef81e5c85a0577
wysiwyng <wysiwyng@users.noreply.github.com>
2015-10-19 20:05:05 +0200

    Update robot.py

--- mai_sr14/robot.py
+++ mai_sr14/robot.py
@@ -1,3 +1,18 @@
+#    Copyright (C) 2105  wysiwyng
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from sr import *
 import motor, debug, time, lift, voltaged, anticollisiond, configReader, function, manouvers, maiduino
 from manouvers import ManouverChainException1, ManouverChainException2, ManouverChainException3, ManouverChainException4, ManouverChainException5
